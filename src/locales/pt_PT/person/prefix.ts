7db34bd2a0a90c70cf318240d2920b4f8b4b4aab
ST-DDT <ST-DDT@gmx.de>
2024-12-24 15:44:26 +0100

    chore: fix locale patterns

--- src/locales/pt_PT/person/prefix.ts
+++ src/locales/pt_PT/person/prefix.ts
@@ -1,22 +1,13 @@
 export default {
   female: [
-    'Adv.ª', // Advogada (Lawyer, female)
-    'Arq.ª', // Arquiteta (Architect, female)
-    'Dra.', // Doutora (Doctor, female)
-    'Enf.ª', // Enfermeira (Nurse, female)
-    'Eng.ª', // Engenheira (Engineer, female)
-    'Prof.ª', // Professora (female)
-    'Sra.', // Senhora (Mrs.)
-    'Téc.ª', // Técnica (Technician, female)
+    'Adv.ª',
+    'Arq.ª',
+    'Dra.',
+    'Enf.ª',
+    'Eng.ª',
+    'Prof.ª',
+    'Sra.',
+    'Téc.ª',
   ],
-  male: [
-    'Adv.', // Advogado (Lawyer, male)
-    'Arq.', // Arquiteto (Architect, male)
-    'Dr.', // Doutor (Doctor, male)
-    'Enf.', // Enfermeiro (Nurse, male)
-    'Eng.º', // Engenheiro (Engineer, male)
-    'Prof.', // Professor (male)
-    'Sr.', // Senhor (Mr.)
-    'Téc.', // Técnico (Technician, male)
-  ],
+  male: ['Adv.', 'Arq.', 'Dr.', 'Enf.', 'Eng.º', 'Prof.', 'Sr.', 'Téc.'],
 };
