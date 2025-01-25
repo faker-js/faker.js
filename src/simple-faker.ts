905c9b9a5a3839da6b75deebb2116fa0bfead661
Matt Mayer <matt@matthewmayer.co.uk>
2025-01-25 12:09:34 +0700

    fix other signature

--- src/simple-faker.ts
+++ src/simple-faker.ts
@@ -207,11 +207,11 @@ export class SimpleFaker {
    * // Consistent values for tests:
    * faker.seed([42, 13, 17])
    * faker.number.int(10); // 4
-   * faker.number.int(10); // 8
+   * faker.number.int(10); // 10
    *
    * faker.seed([42, 13, 17])
    * faker.number.int(10); // 4
-   * faker.number.int(10); // 8
+   * faker.number.int(10); // 10
    *
    * // Random but reproducible tests:
    * // Simply log the seed, and if you need to reproduce it, insert the seed here
